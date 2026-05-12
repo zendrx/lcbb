83c03a1c6eeba940cc62b1238d3e50ec6a43d247
ZenDrx <ynwghosted@icloud.com>
2026-05-12 13:07:30 +0100

    Update URL for fetching challenge markdown files

--- src/cli/fetch.cr
+++ src/cli/fetch.cr
@@ -6,7 +6,7 @@ MANIFEST_CONTENTS = {{read_file("src/manifest.json")}}
 def get_challenge(id : String)
   manifest = JSON.parse(MANIFEST_CONTENTS)
   challenge_lvl = manifest[id]
-  url = "https://raw.githubusercontent.com/zendrx/challenges/main/#{challenge_lvl}/#{id}.md"
+  url = "https://raw.githubusercontent.com/zendrx/lcbb/main/#{challenge_lvl}/#{id}.md"
   response = HTTP::Client.get(url)
   if response.status_code == 200
     markdown_content = response.body
